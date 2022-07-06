1c6501dc55c3f5b537055332e4c14010aa7c0b1e
tbrault <heymarin@hotmail.fr>
2022-07-06 22:15:11 +0200

    add handler event

--- password-generator-app/src/app/app.component.ts
+++ password-generator-app/src/app/app.component.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  isIncludedLetters = false;
+  isIncludedNumbers = false;
+  isIncludedSymbols = false;
+  passwordLength = 0;
+
+  onChangeLength(value: string): void {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
+    console.log(this.passwordLength);
+  }
+
+  onChangeUseLetters(): void {
+    this.isIncludedLetters = !this.isIncludedLetters;
+  }
+
+  onChangeUseNumbers(): void {
+    this.isIncludedNumbers = !this.isIncludedNumbers;
+  }
+
+  onChangeUseSymbols(): void {
+    this.isIncludedSymbols = !this.isIncludedSymbols;
+  }
 
   generatePassword(): void {
     this.password = 'MY PASSWORD';
