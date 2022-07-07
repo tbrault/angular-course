bdb84387c67d3bfd8e59555bfdf82092159f19a6
tbrault <heymarin@hotmail.fr>
2022-07-07 15:38:55 +0200

    add properties to app and card components

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      imageUrl: 'assets/tree.jpeg',
+      title: 'Neat Tree',
+      username: '@nature',
+      content: 'Saw the awesome tree',
+    },
+    {
+      imageUrl: 'assets/biking.jpeg',
+      title: 'Biking outside',
+      username: '@biker',
+      content: 'I jump with my bike',
+    },
+    {
+      imageUrl: 'assets/mountain.jpeg',
+      title: 'Snowy mountain',
+      username: '@krasus',
+      content: 'Ouah the mountain',
+    },
+  ];
 }
--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input() imageUrl = '';
+  @Input() title = '';
+  @Input() username = '';
+  @Input() content = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
