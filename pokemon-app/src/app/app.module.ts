6e8e09bdacee91b4f5b3a6b0fca312105ba0425d
tbrault <heymarin@hotmail.fr>
2022-07-12 22:48:39 +0200

    add pipe pokemon-type-color

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirectiveDirective } from './border-card-directive.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirectiveDirective
+    BorderCardDirectiveDirective,
+    PokemonTypeColorPipe
   ],
   imports: [
     BrowserModule,
