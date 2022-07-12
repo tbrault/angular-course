9200784a6e83e0ad89d12d24fb30535a8b962c3e
tbrault <heymarin@hotmail.fr>
2022-07-12 19:28:59 +0200

    add directive

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirectiveDirective } from './border-card-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderCardDirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ pokemon-app/src/app/border-card-directive.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
+
+@Directive({
+  selector: "[appBorderCardDirective]",
+})
+export class BorderCardDirectiveDirective {
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number = 180;
+
+  constructor(private el: ElementRef) {
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
+  }
+
+  @Input("appBorderCardDirective") borderColor: string;
+
+  @HostListener("mouseenter") onMouseEnter() {
+    this.setBorder(this.borderColor || this.defaultColor);
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.setBorder(this.initialColor);
+  }
+
+  private setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  private setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  }
+}
