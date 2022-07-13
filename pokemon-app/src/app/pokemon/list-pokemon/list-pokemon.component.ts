9b6444f0b03a49764d2c6268390e2987382c2a5a
tbrault <heymarin@hotmail.fr>
2022-07-13 20:01:33 +0200

    add services

--- pokemon-app/src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ pokemon-app/src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,17 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { POKEMONS } from "../mock-pokemon-list";
 import { Pokemon } from "../pokemon";
+import { PokemonService } from "../pokemon.service";
 
 @Component({
   selector: "app-list-pokemon",
   templateUrl: "./list-pokemon.component.html",
   styles: [],
 })
-export class ListPokemonComponent {
-  pokemonList: Pokemon[] = POKEMONS;
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonList = this.pokemonService.getPokemonList();
+  }
 
   displayPokemon(pokemon: Pokemon) {
     this.router.navigate(["/pokemons", pokemon.id]);
--- pokemon-app/src/app/pokemon/pokemon.module.ts
+++ pokemon-app/src/app/pokemon/pokemon.module.ts
@@ -5,6 +5,7 @@ import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { RouterModule, Routes } from "@angular/router";
+import { PokemonService } from "./pokemon.service";
 
 const pokemonRoutes: Routes = [
   { path: "pokemons", component: ListPokemonComponent },
@@ -18,5 +19,6 @@ const pokemonRoutes: Routes = [
     ListPokemonComponent,
   ],
   imports: [CommonModule, RouterModule.forChild(pokemonRoutes)],
+  providers: [PokemonService],
 })
 export class PokemonModule {}
